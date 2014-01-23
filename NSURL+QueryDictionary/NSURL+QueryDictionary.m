cc2123ba433200cea027da7ad125224bcd897223
Jonathan Crooke <joncrooke@gmail.com>
2014-01-23 23:34:17 +0000

    Update for empty values

--- NSURL+QueryDictionary/NSURL+QueryDictionary.m
+++ NSURL+QueryDictionary/NSURL+QueryDictionary.m
@@ -52,11 +52,17 @@ static NSString *const kFragmentBegin   = @"#";
   NSMutableDictionary *mute = @{}.mutableCopy;
   for (NSString *query in [self componentsSeparatedByString:kQuerySeparator]) {
     NSArray *components = [query componentsSeparatedByString:kQueryDivider];
-    if (components.count == 2) {
-      NSString *key = [components[0] stringByRemovingPercentEncoding];
-      NSString *value = [components[1] stringByRemovingPercentEncoding];
-      mute[key] = value;
+    NSString *key = nil;
+    id value = nil;
+    if (components.count > 0) {
+      key = [components[0] stringByRemovingPercentEncoding];
     }
+    if (components.count > 1) {
+      value = [components[1] stringByRemovingPercentEncoding];
+    } else {
+      value = [NSNull null];
+    }
+    mute[key] = value;
   }
   return mute.count ? mute.copy : nil;
 }
@@ -70,13 +76,17 @@ static NSString *const kFragmentBegin   = @"#";
 - (NSString*) URLQueryString {
   NSMutableString *queryString = @"".mutableCopy;
   for (NSString *key in self.allKeys) {
-    NSString *value = [[self[key] description]
-                       stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    id rawValue = self[key];
+    NSString *value = nil;
+    // beware of empty or null
+    if (!(rawValue == [NSNull null] || ![rawValue description].length)) {
+      value = [[self[key] description] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    }
     [queryString appendFormat:@"%@%@%@%@",
      queryString.length ? kQuerySeparator : @"",    // appending?
      [key stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding],
-     kQueryDivider,
-     value];
+     value ? kQueryDivider : @"",
+     value ? value : @""];
   }
   return queryString.length ? queryString.copy : nil;
 }
